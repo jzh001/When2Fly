5a80adde4cdec70bca0e71c2a603a854cf75af9b
Alexander Hu <alexanderhu77@ucla.edu>
2025-05-13 20:06:52 -0700

    implemented basic home and login screen

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div className="home">
+        <h1>Home Screen</h1>
+        <button>Log In</button>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
