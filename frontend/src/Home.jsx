8170c840a0df64cd4235650734e44a355103013b
Alexander Hu <alexanderhu77@ucla.edu>
2025-05-18 18:32:04 -0700

    added description of webapp

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -12,6 +12,14 @@ function Home() {
           <img className="airplane" src="/airplane.webp" alt="Airplane flying" />
           <img className="airplane delay" src="/airplane.webp" alt="Airplane flying" />
         </div>
+
+        <div className="body">
+          <h2>Find the best time to fly</h2>
+          <p>
+            When2Fly is your go-to app for UCLA students looking to save on travel to and from Los Angeles International Airport (LAX). 
+            Inspired by the ease of When2meet, our platform connects you with fellow Bruins whose flight times match yours, making it simple to share an Uber and split the cost. Say goodbye to the hassle of informal group chats and hello to a smarter, more affordable way to travel!
+          </p>
+        </div>
       </>
     );
   }
