6e64e67406e4b672c62e778bf38ec54e8aebfc2b
jzh001 <jiangzhsg@gmail.com>
2025-06-02 17:58:22 -0700

    removed dummy flights

--- backend/data/flightsData.js
+++
@@ -1,9 +0,0 @@
-// Dummy flight data
-const flights = [
-    { id: 1, name: "Flight A", time: "10:00 AM", toLAX: true },
-    { id: 2, name: "Flight B", time: "3:30 PM", toLAX: false },
-    { id: 3, name: "Flight C", time: "12:15 PM", toLAX: true },
-    { id: 4, name: "Flight D", time: "6:00 PM", toLAX: false },
-];
-
-module.exports = flights;
